890e2ad3ed5d0ab0f346182d11e1ed4f141d1582
Miss-Melissa <melissahh@live.se>
2024-11-19 11:26:27 +0100

    I added a link to the header.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
-      <h2>Header</h2>
+    <div className='header'>
+
+      <div className="logo">
+        <Link className='link-header-logo' to='/'><h3>Header</h3></Link>
+      </div>
+
+      <div className="header-right">
+        <Link className='link-header' to='/'>Home</Link>
+        <Link className='link-header' to='/contact'>Contact</Link>
+        <Link className='link-header' to='/about'>About</Link>
+      </div>
+
     </div>
   );
 }
